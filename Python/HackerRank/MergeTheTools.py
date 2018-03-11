e036dda25113e37e1d1192178cd394197dab9a11
PatrickSampaio <patricksampaio48@gmail.com>
2018-03-11 14:45:26 -0300

    Create MergeTheTools.py

---
+++ Python/HackerRank/MergeTheTools.py
@@ -0,0 +1,8 @@
+## https://www.hackerrank.com/challenges/merge-the-tools/problem
+
+def merge_the_tools(string, k):
+    chunk_sizes = len(string)//k
+    for i in range(0, len(string), k):
+        print(''.join([list(string[i:i+k])[index] \
+            if list(string[i:i+k])[index] not in list(string[i:i+k])[:index] else '' \
+            for index in range(0, len(list(string[i:i+k])))]))
