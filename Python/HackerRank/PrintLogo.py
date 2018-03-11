27f8b86b1a9e2fa5fb4191869ee5cddfed462943
PatrickSampaio <patricksampaio48@gmail.com>
2018-03-11 14:16:49 -0300

    Create PrintLogo.py

---
+++ Python/HackerRank/PrintLogo.py
@@ -0,0 +1,24 @@
+#Replace all ______ with rjust, ljust or center. 
+
+thickness = int(input()) #This must be an odd number
+c = 'H'
+
+#Top Cone
+for i in range(thickness):
+    print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+
+#Top Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).rjust((thickness*4)-(thickness+1)//2))
+
+#Middle Belt
+for i in range((thickness+1)//2):
+    print((c*thickness*5).rjust((thickness*6)-(thickness+1)//2))    
+
+#Bottom Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).rjust((thickness*4)-(thickness+1)//2))
+
+#Bottom Cone
+for i in range(thickness):
+    print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))  
